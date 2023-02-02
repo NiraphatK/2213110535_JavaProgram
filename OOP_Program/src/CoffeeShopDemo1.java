3cb31549b96855b097007db2e57b294897a4184d
NiraphatK <niraphat23@gmail.com>
2023-02-02 15:59:42 +0700

    Practice2

--- OOP_Program/src/CoffeeShopDemo1.java
+++ OOP_Program/src/CoffeeShopDemo1.java
@@ -1,9 +1,15 @@
 import javax.swing.JOptionPane;
 
 public class CoffeeShopDemo1 {
+	public static void main(String[] args) {
+		
+	
 	Barista staff1 = new Barista("Leon",'M');
 	Barista staff2 = new Barista("Claire",'F');
 	
-	int chk = Integer.parseInt(JOptionPane.showInputDialog(null,"Type 1 [Hot] :\nType 2 [Cold] :"));
+	int chkDrink = Integer.parseInt(JOptionPane.showInputDialog(null,"Type 1 [Hot] :\nType 2 [Cold] :"));
+	int chkType = Integer.parseInt(JOptionPane.showInputDialog(null,"Type 1 [Americano] :\nType 2 [Espresso] :\nType 3 [Cappuccino] :"));
+	char inputSize = JOptionPane.showInputDialog(null,"Input size").toLowerCase().charAt(0);
 	
+	}	
 }
